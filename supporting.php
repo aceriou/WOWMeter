4e9009f7969b4a38a030b403d7d7ff15d754fcc6
Preston Cammarata <prestotron7@gmail.com>
2016-03-27 13:16:42 -0400

    Update supporting.php

--- supporting.php
+++ supporting.php
@@ -16,7 +16,7 @@
 	<tr><td><a href="#" onclick="window.opener.location.href='http://retranet.com;window.close()">Retranet</a></td></tr>
 	<tr><td><a href="#" onclick="window.opener.location.href='http://socialneko.com';window.close()">SocialNeko</a></td></tr>
 	</table></p>
-	<p>Supporting websites are websites which supports a special BBCode made for WOWMeter. Contact preston@cammarata.info to learn how you can support WOWMeter too!</p>
+	<p>Supporting websites are websites which supports a special BBCode made for WOWMeter. Contact <?php echo CONTACT; ?> to learn how you can support WOWMeter too!</p>
 	<a href="javascript:window.close();window.location='/';void(0)" class="button blue ico-right right">Go back</a>
 	</div>
 <?php $no_script = 1; require_once 'main.php'; ?>
