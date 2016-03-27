4e0acb6d20c6ee54db8c2ee3615e1d71dba03e07
Preston Cammarata <prestotron7@gmail.com>
2016-03-27 13:11:49 -0400

    Create supporting.php

---
+++ supporting.php
@@ -0,0 +1,22 @@
+<?php $login_required = 1; require_once 'header.php'; ?>
+
+	<style type="text/css">
+	.box {
+		min-width: 75%
+	} 
+	</style>
+
+	<div class="box small no-title headline">
+		<h2>Supporting websites</h2>
+	</div>
+	<div class="box small no-title" style="text-align:left;width: 80%">
+	<p>Currently, supporting websites are:
+	<table>
+  <tr><td><a href="#" onclick="window.opener.location.href='http://misdew.com';window.close()">Misdew</a></td></tr>
+	<tr><td><a href="#" onclick="window.opener.location.href='http://retranet.com;window.close()">Retranet</a></td></tr>
+	<tr><td><a href="#" onclick="window.opener.location.href='http://socialneko.com';window.close()">SocialNeko</a></td></tr>
+	</table></p>
+	<p>Supporting websites are websites which supports a special BBCode made for WOWMeter. Contact preston@cammarata.info to learn how you can support WOWMeter too!</p>
+	<a href="javascript:window.close();window.location='/';void(0)" class="button blue ico-right right">Go back</a>
+	</div>
+<?php $no_script = 1; require_once 'main.php'; ?>
