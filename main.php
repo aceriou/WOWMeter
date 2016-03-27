69670dddda18734cccc2221a529351e80574112b
Preston Cammarata <prestotron7@gmail.com>
2016-03-27 18:18:32 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
-<!--<a href="https://github.com/Acerio/WOWMeter"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>-->
+<<a href="https://github.com/aceriou/WOWMeter"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>
 
 <div class="box small no-title headline" style="padding: 5px;display: block">
-        <strong>March 22nd 2016:</strong> This is some news. 
+        <strong>March 27th 2016:</strong> WOWMeter is back, with a brand new codebase found on the GitHub page :) 
 </div>
 
 <?php
@@ -327,7 +327,7 @@ if ( $no_script != 1 ) {
   <div class="box col-md-4 col-md-offset-4">
         <div class="title">About</div>
     
-        WOWMeter is a loosely-tied community where you give and recieve "wows". When someone clicks on your signature, you get a wow, and vise versa. The more wows, the cooler you are.
+        WOWMeter is a loosely-tied community where you give and recieve "wows". When someone clicks on your signature, you get a wow, and vise versa. The more wows, the cooler you are. WOWMeter spreads across a variety of communities, especially in forums.
         
         <br />
         <br />
