05abe91002db6a016cb241b2a46a1667fa324974
Preston Cammarata <prestotron7@gmail.com>
2016-03-27 13:17:06 -0400

    Create tos.php

---
+++ tos.php
@@ -0,0 +1,19 @@
+<?php require_once 'header.php'; ?>
+
+	<div class="box small no-title headline">
+	<h1>WOWMeter Terms of Service</h1>
+	<p>This agreement was last modified on March 27, 2016.</p>
+	</div>
+			<div class="box small no-title" style="text-align:left;min-width:60%">
+			<p>By accessing or using WOWMeter in any manner, including, but not limited to, visiting or browsing WOWMeter or contributing content or other materials to WOWMeter, you agree to be bound by these Terms of Service. Capitalized terms are defined in this agreement.</p>
+			<p><strong>Intellectual Property</strong><br />WOWMeter and its original content, features and functionality are owned by WOWMeter and are protected by international copyright, trademark, patent, trade secret and other intellectual property or proprietary rights laws.</p>
+			<p><strong>Termination</strong><br />We may terminate your access to WOWMeter, without cause or notice, which may result in the forfeiture and destruction of all information associated with you. All provisions of this agreement that by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.</p>
+			<p><strong>Links to other websites</strong><br />WOWMeter may contain links to third-party websites that are not owned or controlled by WOWMeter.</p>
+			<p>WOWMeter has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party websites or services. We strongly advise you to read the terms and conditions and privacy policy of any third-party website that you visit.</p>
+			<p><strong>Governing law</strong><br />This agreement (and any further rules, polices, or guidelines incorporated by reference) shall be governed and construed in accordance with the laws of Rhode Island, USA, without giving effect to any principles of conflicts of law.</p>
+			<p><strong>Changes to this agreement</strong><br />We reserve the right, at our sole discretion, to modify or replace these Terms of Service by posting the updated terms on WOWMeter. Your continued use of WOWMeter after any such changes constitutes your acceptance of the new Terms of Service.</p>
+			<p>Please review this agreement periodically for changes. If you do not agree to any of this agreement or any changes to this agreement, do not use, access or continue to access WOWMeter or discontinue any use of WOWMeter immediately.</p>
+			<p><strong>Contact us</strong><br />If you have any questions about this agreement, please contact us at <a href="mailto:<?php echo CONTACT; ?>"><?php echo CONTACT; ?></a>.</p>
+			<p><a href="/">Go back</a></p>
+				</div>
+<?php $no_script = 1; require_once 'main.php'; ?>
