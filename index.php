3662a8d3c0ad9e74af831b73024df1b8db621c69
Preston Cammarata <prestotron7@gmail.com>
2016-03-27 22:06:21 -0400

    Add random strings

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 require_once 'header.php';
+
+$random_strings = array ( 'Hi there', 'Hey', 'What\'s up', 'Hello', 'Yo' );
+$rs_key         = array_rand ( $random_strings );
+
+
 ?>
 	<div class="box small no-title headline">
     
@@ -7,7 +12,7 @@ require_once 'header.php';
 		if ( $member_access == 0 ) {
 		  echo "<h2>Welcome to " . SITE_NAME . "!</h2><h6>Create an account to get your very own " . SITE_NAME . ".</h6>";
 		} else {
-		  echo "<h2>Hi there, $username!</h2>";
+		  echo "<h2>" . $random_strings[$rs_key] . ", $username!</h2>";
 		}
     
     ?>
