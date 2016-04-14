72c24cacb4853add085e9e5c84348736b60d560d
Preston Cammarata <prestotron7@gmail.com>
2016-04-14 09:52:51 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <!--<a href="https://github.com/aceriou/WOWMeter"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>-->
 
 <div class="box small no-title headline" style="padding: 5px;display: block">
-        <strong>March 31st 2016:</strong> WOWRanks were added omg. Japanese signature is not working for the time being.
+        <strong>April 14th 2016:</strong> Japanese font fixed.
 </div>
 
 <?php
